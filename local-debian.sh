b241957ad8d3dfd6b3ff71a5eebfa527472a2e2b
Josh Cox <josh@webhosting.coop>
2016-03-06 10:24:02 -0600

    quieter

--- local-debian.sh
+++ local-debian.sh
@@ -24,7 +24,7 @@ done
 
 temp=$(mktemp -d)
 OUTPUT_SEMAPHORE=`pwd`/local-$release
-apt-get install -yq debootstrap
+apt-get install -yqq debootstrap
 debootstrap --variant=minbase --include=apt-utils,less,vim,locales,libterm-readline-gnu-perl $release "$temp" http://http.us.debian.org/debian/ 
 echo "deb http://security.debian.org/ $release/updates main" > "$temp/etc/apt/sources.list.d/security.list"
 echo "deb http://ftp.us.debian.org/debian/ $release-updates main" > "$temp/etc/apt/sources.list.d/update.list"
