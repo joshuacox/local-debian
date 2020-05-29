52f2e75818630c0fa9b5da41e2e909d330490ded
Josh Cox <josh@webhosting.coop>
2020-05-28 19:08:09 -0500

    permissive

--- local-debian.sh
+++ local-debian.sh
@@ -25,6 +25,7 @@ done
 temp=$(mktemp -d)
 OUTPUT_SEMAPHORE=`pwd`/local-$release
 apt-get install -yqq debootstrap
+chmod 777 "$temp"
 sudo /usr/sbin/debootstrap --variant=minbase --include=apt-utils,less,vim,locales,libterm-readline-gnu-perl $release "$temp" http://http.us.debian.org/debian/ 
 echo "deb http://security.debian.org/ $release/updates main" > "$temp/etc/apt/sources.list.d/security.list"
 echo "deb http://ftp.us.debian.org/debian/ $release-updates main" > "$temp/etc/apt/sources.list.d/update.list"
